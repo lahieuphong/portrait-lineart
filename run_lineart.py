f30d0510dc7273a31756f7e5a99e2b5cc4946ccb
lahieuphong <hieuphong144@gmail.com>
2025-12-03 10:51:25 +0700

    Update run_lineart.py and add new input image

--- run_lineart.py
+++ run_lineart.py
@@ -4,17 +4,23 @@ import sys
 import subprocess
 
 # chỉnh đường dẫn ảnh đầu vào cho đúng
-INPUT = "data/input/picture_1.jpg"
+INPUT = "data/input/69836bb0-c90e-4a83-85c1-7e12017ee7cf.png"
 
 cmd = [
     sys.executable, "src/portrait_lineart_turtle.py",
     "-i", INPUT,
-    "--fast",
-    "--chaikin", "1",
-    "--cr_samples", "6",
-    "--line_width", "2.5",
-    "--thickness_mode", "fixed",
-    # NOTE: đã bỏ --output_svg để không lưu file tự động
+    "--fast",                      # Bật chế độ vẽ nhanh
+    "--chaikin", "2",              # Làm mượt vừa phải
+    "--cr_samples", "8",           # Tăng độ mượt đường cong
+    "--line_width", "1",           # Nét mảnh
+    "--thickness_mode", "length",  # Nét dài dày hơn
+    "--min_width", "0.2",          # Nét mảnh tối thiểu
+    "--max_width", "1.2",          # Nét dày tối đa
+    "--eps", "0.8",                # Giữ chi tiết
+    "--blur", "0.5",               # Giảm làm mờ
+    "--edge_mul", "0.8",           # Bắt nhiều cạnh hơn
+    "--min_path_len", "5",         # Bỏ nhiễu
+    "--batch", "50",               # Vẽ 50 nét rồi mới cập nhật màn hình
 ]
 
 print("Running:", " ".join(cmd))
